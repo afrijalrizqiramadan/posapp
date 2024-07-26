79a5065f232c19afc42e4498351acf9caab05b60
afrijalrizqiramadan <arizqiramadan@gmail.com>
2024-07-26 10:37:48 +0700

    baru

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,5 @@
+<footer class="c-footer">
+    <div>Admin Pos © {{ date('Y') }} || Developed by <strong><a target="_blank" href="https://asyabab.net">IT</a></strong></div>
+
+    <div class="mfs-auto d-md-down-none">Version <strong class="text-danger">1.0</strong></div>
+</footer>
